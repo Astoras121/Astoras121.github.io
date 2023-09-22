9e95a44fb8ec8a1334b96105694fc108e8caacaf
Website <127028592+Astoras121@users.noreply.github.com>
2023-09-22 17:05:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let percent = 0;
-let otherPercent
-const loader = document.getElementById("CircleLoader");
-let button = document.getElementById("submitButton");
-let label = document.getElementById("Percent")
 
-button.onclick = function(){
-    let value = document.getElementById("inputPercent").value;
-    percent = value + "%";
-    otherPercent = 100 - value + "%";
-    loader.style.background = `linear-gradient(to top, green 0% ${percent}, transparent 0% ${otherPercent})`
-    label.textContent = percent;
-}
