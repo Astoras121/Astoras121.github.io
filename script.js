b1b40aca1a0db985265d6253be6e632557485bdc
Website <127028592+Astoras121@users.noreply.github.com>
2023-09-22 17:28:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const button = document.getElementById("animationButton");
+let circle = document.getElementById("roundCircle");
+let percent = 0;
+button.onclick = function(){
+    percent += 5;
+    let transparentPercent = 100 - percent;
+    let newPercent = percent + "%";
+    let newTransparentPercent = transparentPercent + "px";
+    circle.style.background = `linear-gradient(to bottom, transparent 0% ${newTransparentPercent},#7d66b6 0% ${newPercent})`;
+    console.log(newPercent);
+    if(percent == 100){
+        percent = 0;
+    }
+}
