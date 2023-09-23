d3a48b783f9f20f814e86a1c6df6437263ac5030
Website <127028592+Astoras121@users.noreply.github.com>
2023-09-23 14:08:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const button = document.getElementById("animationButton");
-let circle = document.getElementById("roundCircle");
-let percent = 0;
-button.onclick = function(){
-    percent += 5;
-    let transparentPercent = 100 - percent;
-    let newPercent = percent + "%";
-    let newTransparentPercent = transparentPercent + "px";
-    circle.style.background = `linear-gradient(to bottom, transparent 0% ${newTransparentPercent},#7d66b6 0% ${newPercent})`;
-    console.log(newPercent);
-    if(percent == 100){
-        percent = 0;
+let buttonDrink = document.getElementById("drink");
+let buttonEat = document.getElementById("eat");
+let eatStroke = document.getElementById("percentStroke2");
+let drinkStroke = document.getElementById("percentStroke");
+let foodPercent = 0;
+let drinkPercent = 0;
+
+class Food{
+    constructor(foodPercent){
+        this.foodPercent = foodPercent;
+    }
+
+    addPercent(){
+        this.foodPercent += 10;
+    }
+}
+
+class Drink{
+    constructor(drinkPercent){
+        this.drinkPercent = drinkPercent;
+    }
+
+    addPercent(){
+        this.drinkPercent += 10;
+    }
+}
+
+let Food1 = new Food(foodPercent)
+let Drink1 = new Drink(drinkPercent);
+
+buttonEat.onclick = function(){
+    Food1.foodPercent += 10;
+    console.log(Food1.foodPercent);
+    eatStroke.style.background = `conic-gradient(purple ${Food1.foodPercent}deg,transparent 0deg)`;
+
+    if(Food1.foodPercent == 360){
+        Food1.foodPercent = 0;
+    }
+}
+
+buttonDrink.onclick = function(){
+    Drink1.drinkPercent += 10;
+    console.log(Drink1.drinkPercent);
+    drinkStroke.style.background = `conic-gradient(purple ${Drink1.drinkPercent}deg,transparent 0deg)`;
+
+    if(Drink1.drinkPercent == 360){
+        Drink1.drinkPercent = 0;
     }
 }
