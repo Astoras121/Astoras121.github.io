59209cf1649c322c5a8dfc0560cd2a3b374c0e64
GadgetsAndMore <127028592+Astoras121@users.noreply.github.com>
2023-09-30 12:37:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,160 @@
-let buttonDrink = document.getElementById("drink");
-let buttonEat = document.getElementById("eat");
-let eatStroke = document.getElementById("percentStroke2");
-let drinkStroke = document.getElementById("percentStroke");
-let foodPercent = 0;
-let drinkPercent = 0;
+for (let i = 1; i <= 11; i++) {
+  let div = document.getElementById(`div${i}`);
+  div.style.height = "70px";
+  div.style.width = "270px";
+  div.style.background = "var(--clr)";
+  div.style.transform = "skewY(-5deg)";
+  div.style.display = "flex";
+  div.style.justifyContent = "center";
+  div.style.alignItems = "center";
 
-class Food{
-    constructor(foodPercent){
-        this.foodPercent = foodPercent;
-    }
+  const computedStyle = getComputedStyle(div);
+  const langValue = computedStyle.getPropertyValue("--lang");
+  const image1 = computedStyle.getPropertyValue("--img");
 
-    addPercent(){
-        this.foodPercent += 10;
-    }
+  let language = document.createElement("div");
+  language.style.width = "40px";
+  language.style.height = "30px";
+  language.style.background = "black";
+  language.style.position = "relative";
+  language.style.left = "45px";
+  div.appendChild(language);
+
+  if (langValue == "british") {
+      language.style.backgroundImage = `url("uk.jpeg")`;
+      language.style.backgroundSize = "40px 30px";
+  }
+
+  if (langValue == "spanish") {
+    language.style.backgroundImage = `url("spain.jpeg")`;
+    language.style.backgroundSize = "40px 30px";
 }
 
-class Drink{
-    constructor(drinkPercent){
-        this.drinkPercent = drinkPercent;
-    }
 
-    addPercent(){
-        this.drinkPercent += 10;
-    }
+  if (langValue == "german") {
+      language.style.backgroundImage = `url("germany.jpeg")`;
+      language.style.backgroundSize = "40px 30px";
+  }
+
+  if (langValue == "serbian") {
+      language.style.backgroundImage = `url("serbia.jpeg")`;
+      language.style.backgroundSize = "40px 30px";
+  }
+
+  if (langValue == "italian") {
+      language.style.backgroundImage = `url("italy.jpeg")`;
+      language.style.backgroundSize = "40px 30px";
+  }
+
+  if (langValue == "french") {
+      language.style.backgroundImage = `url("france.jpeg")`;
+      language.style.backgroundSize = "40px 30px";
+  }
+
+  if (langValue == "portugal") {
+    language.style.backgroundImage = `url("portugal.jpeg")`;
+    language.style.backgroundSize = "40px 30px";
 }
 
-let Food1 = new Food(foodPercent)
-let Drink1 = new Drink(drinkPercent);
+  var imgElements = div.querySelectorAll("img");
 
-buttonEat.onclick = function(){
-    Food1.foodPercent += 10;
-    console.log(Food1.foodPercent);
-    eatStroke.style.background = `conic-gradient(purple ${Food1.foodPercent}deg,transparent 0deg)`;
+  imgElements.forEach(function (img) {
+      if (image1 == "boat") {
+          img.src = "boat.png";
+          img.style.position = "relative";
+          img.style.right = "100px";
+          img.style.width = "80px";
+      }
+  });
 
-    if(Food1.foodPercent == 360){
-        Food1.foodPercent = 0;
+  imgElements.forEach(function (img) {
+    if (image1 == "car") {
+        img.src = "car.png";
+        img.style.position = "relative";
+        img.style.right = "100px";
+        img.style.width = "80px";
     }
-}
+});
 
-buttonDrink.onclick = function(){
-    Drink1.drinkPercent += 10;
-    console.log(Drink1.drinkPercent);
-    drinkStroke.style.background = `conic-gradient(purple ${Drink1.drinkPercent}deg,transparent 0deg)`;
-
-    if(Drink1.drinkPercent == 360){
-        Drink1.drinkPercent = 0;
+  imgElements.forEach(function (img) {
+    if (image1 == "car2") {
+      img.src = "car2.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
     }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "car3") {
+      img.src = "car3.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "tank") {
+      img.src = "tank.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "helicopter") {
+      img.src = "helicopter.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+      img.style.height = "50px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "car4") {
+      img.src = "car4.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "car5") {
+      img.src = "car5.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "van") {
+      img.src = "van.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "moneytruck") {
+      img.src = "moneytruck.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
+
+  imgElements.forEach(function (img) {
+    if (image1 == "car6") {
+      img.src = "car6.png";
+      img.style.position = "relative";
+      img.style.right = "100px";
+      img.style.width = "80px";
+    }
+  });
 }
