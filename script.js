0728d37d1ddb2ceebeae90770ae9e00ca0adf5d1
GadgetsAndMore <127028592+Astoras121@users.noreply.github.com>
2023-10-11 18:19:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 const border = document.getElementById("border");
-let i = 0;
 let color = "white";
+let i = 0;
 
 function animate() {
-    if (i == 340){
-        color = "#333"
-    }
-
-    if(i == 0){
-        color = "white";
-    }
-
-    i = (i + 1) % 360;
+    i += 1;
     console.log(i);
     border.style.background = `conic-gradient(${color} ${i}deg, transparent ${i}deg)`;
     requestAnimationFrame(animate);
 }
-
 animate();
